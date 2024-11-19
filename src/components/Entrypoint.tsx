d953b895c6c8e23d925178498d6dd989186444b8
Juliya Lipatova <575@ukr.net>
2024-11-19 12:33:50 +0100

    deleted cards added

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -5,10 +5,15 @@ import { Card } from "./List";
 import { Spinner } from "./Spinner";
 
 export const Entrypoint = () => {
+
+  const [isDeletedCardsOpen, setIsDeletedCardsOpen] = useState<boolean>(false)
  
   const listQuery = useGetListData();
-  const {visibleCards, setVisibleCards, deletedCards, setDeletedCards} = useStoreCards()
+  const {visibleCards, setVisibleCards, deletedCards} = useStoreCards()
+  const deletedCount = deletedCards.length
 
+  const isDisable = deletedCount <=0
+  
   // TOOD
   // const deletedCards: DeletedListItem[] = [];
 
@@ -31,6 +36,10 @@ export const Entrypoint = () => {
     }
   }, [listQuery.data, listQuery.isLoading, visibleCards.length]);
 
+  if (listQuery.isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div className="flex gap-x-16">
       <div className="w-full max-w-xl">
@@ -43,20 +52,33 @@ export const Entrypoint = () => {
       </div>
       <div className="w-full max-w-xl">
         <div className="flex items-center justify-between">
-          <h1 className="mb-1 font-medium text-lg">Deleted Cards (0)</h1>
-          <button
-            disabled
-            className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1"
+          <h1 className="mb-1 font-medium text-lg">Deleted Cards {deletedCount}</h1>
+          <button onClick={() => setIsDeletedCardsOpen(!isDeletedCardsOpen)}
+            disabled={isDisable}
+            className={`text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1 ml-10 ${
+              isDisable ? "cursor-not-allowed" : "cursor-pointer"
+            }`}
           >
             Reveal
           </button>
         </div>
         <div className="flex flex-col gap-y-3">
-          {/* {deletedCards.map((card) => (
-            <Card key={card.id} card={card} />
-          ))} */}
+        {isDeletedCardsOpen && deletedCards.length > 0 && 
+    deletedCards.map((card) => (
+      <Card 
+        key={card.id} 
+        title={card.title} 
+        id={card.id} 
+        description={card.description} 
+        isOpenDescription={card.isOpenDescription} 
+      />
+    ))
+  }
         </div>
       </div>
     </div>
   );
 };
+
+
+//   <Card key={card.id} card={card} />
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -13,19 +13,22 @@ type CardProps = {
 
 export const Card: FC<CardProps> = ({id, title, description, isOpenDescription}) => {
 
-  const {deleteCard} = useStoreCards()
+  const {deleteCard, openDescription} = useStoreCards()
+ 
   return (
     <div className="border border-black px-2 py-1.5">
       <div className="flex justify-between mb-0.5">
         <h1 className="font-medium">{title}</h1>
         <div className="flex">
-          <ExpandButton onAction={() =>{}}>
+          <ExpandButton onAction={() =>{openDescription(id)}}>
             <ChevronUpIcon />
           </ExpandButton>
           <DeleteButton onAction={()=>{deleteCard(id)}} />
         </div>
       </div>
-    {isOpenDescription &&  <p className="text-sm">{description}</p>}
+    {isOpenDescription &&  <p  className={`text-sm transition-all duration-300 ease-in-out overflow-hidden ${
+      isOpenDescription ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+    }`}>{description}</p>}
     </div>
   );
 };
